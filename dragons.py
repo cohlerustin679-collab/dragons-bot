ed2b2aa3d2e81714a12f61e04c9fc5bf9c00df0b
cohlerustin679-collab <cohlerustin679@gmail.com>
2026-06-07 01:32:59 +0530

    Update dragons.py

--- dragons.py
+++ dragons.py
@@ -1,8 +1,15 @@
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    MessageHandler,
+    ContextTypes,
+    filters,
+)
 
-TOKEN =  "8733850331:AAG1ZIEUUhuc2t1TnPOtsJOkO6-YGFI-UgE"
+TOKEN = "8733850331:AAG1ZIEUUhuc2t1TnPOtsJOkO6-YGFI-UgE"
 
-IMAGE_FILE = "/storage/emulated/0/Pictures/AyuGram/IMG_20260605_224549_767.jpg"
+IMAGE_URL = "https://i.ibb.co/9HQDRLVT/file-00000000d5b47206bc20556356c90186.png"
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -16,15 +23,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         resize_keyboard=True
     )
 
-    with open(IMAGE_FILE, "rb") as photo:
-        await update.message.reply_photo(
-            photo=photo,
-            caption=(
-                "Three Dragon Heads of Telegram\n\n"
-                "Choose your destination and use the buttons below to levitate."
-            ),
-            reply_markup=reply_markup
-        )
+    await update.message.reply_photo(
+        photo=IMAGE_URL,
+        caption=(
+            "Three Dragon Heads of Telegram\n\n"
+            "Choose your destination and use the buttons below to levitate."
+        ),
+        reply_markup=reply_markup
+    )
 
 
 async def handle_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -34,7 +40,7 @@ async def handle_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             "JAR\n\nhttps://t.me/+jQQHJsHr54oxYzcy"
         )
-     
+
     elif text == "ACP 🔵":
         await update.message.reply_text(
             "ACP\n\nhttps://t.me/AntiChessPropaganda"
@@ -44,6 +50,7 @@ async def handle_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(
             "Labs Chat\n\nhttps://t.me/parmar_Labs"
         )
+
 
 app = Application.builder().token(TOKEN).build()
 
